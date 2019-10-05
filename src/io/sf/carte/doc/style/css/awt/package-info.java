b770e168505130c6bd6e3eef02f3299fbe609c26
Carlos Amengual <carlosame644+github@gmail.com>
2019-10-05 18:00:29 +0200

    More informative package-info.

--- src/io/sf/carte/doc/style/css/awt/package-info.java
+++ src/io/sf/carte/doc/style/css/awt/package-info.java
@@ -1,6 +1,24 @@
 /**
- * Helper class for AWT rendering.
+ * Helper classes for AWT rendering.
+ * <h3><code>AWTHelper</code>: create AWT <code>Font</code> and
+ * <code>Color</code> objects</h3>
  * <p>
- * See the Carte rendering project for more AWT implementation classes.
+ * Once you have a computed style, on systems where AWT is available you can
+ * create an AWT font with the <code>createFont</code> static method in the
+ * <code>AWTHelper</code> class:
+ * </p>
+ * 
+ * <pre style="padding: 0.8em;background-color: #f8f5e1">
+CSSComputedProperties style = ...
+java.awt.Font font = AWTHelper.createFont(style);</pre>
+ * <p>
+ * And <code>AWTHelper.getAWTColor</code> method can create AWT colors from any
+ * value, regardless of it coming from computed or declared styles.
+ * <pre style="padding: 0.8em;background-color: #f8f5e1">
+CSSPrimitiveValue cssColor = (CSSPrimitiveValue) style.getPropertyCSSValue("color");
+java.awt.Color color = AWTHelper.getAWTColor(cssColor);</pre>
+ * </p>
+ * <h3><code>AWTStyleDatabase</code></h3>
+ * <p><code>AWTStyleDatabase</code> is a sample style database for AWT.</p>
  */
 package io.sf.carte.doc.style.css.awt;
