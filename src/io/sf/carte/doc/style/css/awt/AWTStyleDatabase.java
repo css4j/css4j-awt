63479d137c3b2420a39369e1ee6d46f701c42590
Carlos Amengual <carlosame644+github@gmail.com>
2021-01-05 14:29:16 +0100

    Bump year to 2021 in copyright notices (and one URL correction).

--- src/io/sf/carte/doc/style/css/awt/AWTStyleDatabase.java
+++ src/io/sf/carte/doc/style/css/awt/AWTStyleDatabase.java
@@ -1,11 +1,11 @@
 /*
 
- Copyright (c) 2005-2020, Carlos Amengual.
+ Copyright (c) 2005-2021, Carlos Amengual.
 
  SPDX-License-Identifier: BSD-3-Clause
 
  Licensed under a BSD-style License. You can find the license here:
- https://carte.sourceforge.io/LICENSE.txt
+ https://css4j.github.io/LICENSE.txt
 
  */
 
