e3deca2a3396a94f0c94a7724b9c819f5fbbd00d
Carlos Amengual <carlosame644+github@gmail.com>
2024-01-04 12:18:13 +0100

    Update the Javadoc package description

--- src/io/sf/carte/doc/style/css/awt/package-info.java
+++ src/io/sf/carte/doc/style/css/awt/package-info.java
@@ -1,6 +1,11 @@
 /**
- * Helper class for AWT rendering.
+ * Helper classes for AWT rendering.
  * <p>
- * See the Carte rendering project for more AWT implementation classes.
+ * See <a href=
+ * "https://github.com/css4j/echosvg/blob/master/echosvg-transcoder/src/main/java/io/sf/carte/echosvg/transcoder/util/CSSTranscodingHelper.java"
+ * target="_blank">CSSTranscodingHelper</a> in the
+ * <a href="https://github.com/css4j/echosvg/wiki" target="_blank">EchoSVG
+ * project</a> for a usage example.
+ * </p>
  */
 package io.sf.carte.doc.style.css.awt;
